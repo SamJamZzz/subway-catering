15179bc76ddf4ace33acd28bf07f837048262238
Sam Julian <samjulian6@gmail.com>
2023-04-03 23:36:22 -0700

    Improve and finish styling for Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <footer>
       <a href="/">
-        <img src="https://wecatersubs.com/wp-content/uploads/2020/05/subway-logo-new.png"
+        <img className='footer-logo' src="https://wecatersubs.com/wp-content/uploads/2020/05/subway-logo-new.png"
              alt="WeCaterSubs.com"
         />
       </a>
       <p>&#169; 2020 WeCaterSubs.com. All rights reserved | Privacy Policy | 6626B East Hastings Street, Unit B Kensington Square Plaza, Burnaby V5B 1S2</p>
       <a href="https://www.facebook.com/SubwayCanada">
-        <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="facebook" />
+        <img className='fb-logo' src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="facebook" />
       </a>
     </footer>
   )
