27971135a18c0f7335394465d6a5c5a8f90129b1
Sam Julian <samjulian6@gmail.com>
2023-04-03 23:35:54 -0700

    Improve and finish styling for Contact component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <>
-      <section>
-        <div>
-          <h2>Contact information</h2>
+    <div className='contact-content'>
+      <section className='contact'>
+        <div className='contact-info'>
+          <h2>Contact Information</h2>
           <h3>Address:</h3>
           <p>
             6626b Hastings St <br />
@@ -14,13 +15,13 @@ const Contact = () => {
           </p>
           <h3>Phone:</h3>
           <p>604-299-0449</p>
-          <iframe title='6626b Hastings St, Burnaby, BC V5B 1S2' width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=6626b%20Hastings%20St%20Unit%20B%20Kensington%20Square%20Plaza+(We%20Cater%20Subs)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+          <iframe title='6626b Hastings St, Burnaby, BC V5B 1S2' width="100%" height="300" frameborder="0" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=6626b%20Hastings%20St%20Unit%20B%20Kensington%20Square%20Plaza+(We%20Cater%20Subs)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
             <a href="https://www.maps.ie/distance-area-calculator.html">
               measure area map
             </a>
           </iframe>
         </div>
-        <div>
+        <div className='contact-form'>
           <h2>Get in touch!</h2>
           <h4>We're happy to hear from you. Please send us your contact details below.</h4>
           <form action="/contact-us-subway" method='POST'>
@@ -52,7 +53,7 @@ const Contact = () => {
           </form>
         </div>
       </section>
-    </>
+    </div>
   )
 };
 
